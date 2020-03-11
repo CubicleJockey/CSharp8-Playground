94f7c2cf732cb626a4ec09ecab61a6f3a4829028
adavis <cubicle.jockey@live.com>
2020-03-11 00:39:12 -0700

    Inteface default method

---
+++ CSharp8Playground/CSharp8Playground/DefaultInterfacesMethods.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSharp8Playground
+{
+    [TestClass]
+    public class DefaultInterfacesMethods
+    {
+        [TestMethod]
+        public void InheritedDefaultMethod()
+        {
+            IVehicle car = new Car("Toyota Highlander", 4);
+            car.CurrentMph.Should().Be(0);
+            
+            car.IncrementCurrentMph(); //<----IncrementCurrentMph was defined as a default method in IVehicle
+            car.CurrentMph.Should().Be(1);
+            
+            car.IncrementCurrentMph(15);
+            car.CurrentMph.Should().Be(16);
+        }
+        
+    }
+
+    public interface IVehicle
+    {
+        string Name { get; }
+        int NumberOfWheels { get; }
+        int CurrentMph { get; set; }
+
+        //Introducing default methods to interfaces
+        void IncrementCurrentMph(int increase = 1)
+        {
+            CurrentMph += increase;
+        }
+    }
+    
+    /// <summary>
+    /// Inherits from IVehicle, which defines a default method IncrementCurrentMph
+    /// </summary>
+    public class Car : IVehicle
+    {
+        #region Implementation of IVehicle
+
+        /// <inheritdoc />
+        public string Name { get; }
+
+        /// <inheritdoc />
+        public int NumberOfWheels { get; }
+
+        /// <inheritdoc />
+        public int CurrentMph { get; set; }
+
+        #endregion
+
+        public Car(string name, int numberOfWheels)
+        {
+            if(string.IsNullOrWhiteSpace(name)) { throw new ArgumentException(nameof(name)); }
+            Name = name;
+            NumberOfWheels = numberOfWheels;
+        }
+    }
+}
--- CSharp8Playground/CSharp8Playground/ReadonlyMembers.cs
+++ CSharp8Playground/CSharp8Playground/ReadonlyMembers.cs
@@ -1,13 +1,54 @@
+using System;
+using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace CSharp8Playground
 {
+    /// <summary>
+    /// 'readonly' can be applied to the members of a struct.
+    /// A readonly struct member indicates that it does not modify state.
+    /// </summary>
     [TestClass]
     public class ReadonlyMembers
     {
         [TestMethod]
-        public void TestMethod1()
+        public void CallingAReadonlyStructMember()
         {
+            var playerLocation = new Point
+            {
+                X = 45,
+                Y = -32.5
+            };
+
+            playerLocation.Distance.Should().Be(55.509008277936296);
+            playerLocation.ToString().Should().Be("(45, -32.5) is 55.509008277936296 from the origin.");
         }
+        
+        /// <summary>
+        /// Struct that contains, new readonly members.
+        /// </summary>
+        public struct Point
+        {
+            public double X { get; set; }
+            public double Y { get; set; }
+
+            #region Readonly Members
+
+            public readonly double Distance => Math.Sqrt(X * X + Y * Y);
+
+            //Distance must also be readonly or a warning is presented.
+            public override readonly string ToString() => $"({X}, {Y}) is {Distance} from the origin."; 
+
+            #endregion Readonly Members
+
+            //Compiler enforces the readonly rule. This will not compile unless
+            //readonly is removed from the method signature.
+            public /*readonly*/ void Translate(int xOffSet, int yOffSet)
+            {
+                X += xOffSet;
+                Y += yOffSet;
+            }
+        }
+
     }
 }
