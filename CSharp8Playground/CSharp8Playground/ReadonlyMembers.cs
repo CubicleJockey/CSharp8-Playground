9d039b17d9d7045bff9836e83d7505ab0992e8f8
adavis <cubicle.jockey@live.com>
2020-03-11 00:13:35 -0700

    Initial Setup to start practicing with C# 8.0

---
+++ CSharp8Playground/CSharp8Playground/ReadonlyMembers.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSharp8Playground
+{
+    [TestClass]
+    public class ReadonlyMembers
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # CSharp8-Playground
- C# 8.0 Playground
+#### C# 8.0 Playground
+* [Documentation](https://docs.microsoft.com/en-us/dotnet/csharp/whats-new/csharp-8)
+
+
